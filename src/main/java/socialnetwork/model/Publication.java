0756596e905ef38cf9c2cc276b1408507ff39131
emilielr <emilie.rognli@gmail.com>
2022-04-21 12:07:13 +0200

    ex.2 storage publications

--- src/main/java/socialnetwork/model/Publication.java
+++ src/main/java/socialnetwork/model/Publication.java
@@ -2,11 +2,31 @@ package socialnetwork.model;
 
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotBlank;
+
+@Entity
 public class Publication {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
+    @Column(nullable = false)
+    @NotBlank
     private String text;
+
+    @Column(nullable = false)
     private Boolean restricted;
+
+    @ManyToOne(optional = false)
     private User user;
+
+    @Column(nullable = false)
     private Date timestamp;
 
     public Integer getId() {
@@ -34,6 +54,10 @@ public class Publication {
     }
 
     public Boolean isRestricted() {
+        return restricted;
+    }
+
+    public Boolean getRestricted() {
         return restricted;
     }
 
